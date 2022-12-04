2e7e89dd04f7640dcbd92d3dc83bb917c91bc31c
Isaac-Andriamiharioa <nyainaisaac@gmail.com>
2022-12-04 16:39:57 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+from models import app, Ame
+from flask import render_template, request, jsonify
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+@app.route('/')
+def login():
+    return "Under maintenance"
+
+@app.route('/ames')
+def get_ames():
+    ames = Ame.query.all()
+    data = []
+    for ame in ames:
+        data.append({
+            'nom': ame.nom,
+            'prenom': ame.prenom
+        })
+    return render_template('ames.html', ames=data)
+
+@app.route('/ames/<id>')
+def get_ame_details(id):
+    data = Ame.query.get(id)
+    return jsonify({
+        'nom': data.nom,
+        'prenom': data.prenom,
+        'bapteme': data.bapteme
+    })
+ 
+@app.route('/ames/create', methods=['GET'])
+def get_page():
+    return render_template('create_ames.html')
+
+@app.route('/ames/create', methods=['POST'])
+def create():
+    data = request.get_json()
+    nom = data['nom']
+    prenom = data['prenom']
+    bapteme = data['bapteme']
+    ame = Ame(nom=nom, prenom=prenom, bapteme=bapteme)
+    ame.insert()
+
+    return jsonify({
+        'success': True,
+        'ameCreated': nom
+    })
+
+@app.route('/ames/<id>/update', methods=['GET'], endpoint='update')
+def get_page(id):
+    data = Ame.query.get(id)
+    return render_template('update_ames.html', data = data)
+
+@app.route('/ames/<id>/update', methods=['POST'], endpoint='updating')
+def update(id):
+    ame = Ame.query.filter(Ame.id == id).one_or_none()
+    data = request.get_json()
+    ame.nom = data['nom']
+    ame.prenom = data['prenom']
+    ame.bapteme = data['bapteme']
+    ame.update()
+
+    return jsonify({
+        'success': True,
+        'ame_updated': ame.nom
+    })
+
+@app.route('/ames/<id>/delete')
+def delete(id):
+    ame = Ame.query.filter(Ame.id == id).one_or_none()
+    ame.delete()
+    return jsonify({
+        'success': True,
+        'ame_deleted': ame.nom
+    })
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://isaac:BELOHA03@localhost:5432/liber'
+
+class Ame(db.Model):
+    __tablename__ = 'ames'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String)
+    prenom = db.Column(db.String)
+    bapteme = db.Column(db.Boolean)
+
+    def __init__(self, nom, prenom, bapteme):
+        self.nom = nom
+        self.prenom = prenom
+        self.bapteme = bapteme
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
